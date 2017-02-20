83bea14f021c331b700fe069191e99a4532a33fc
Armin Ronacher <armin.ronacher@active-4.com>
2017-02-20 16:23:34 +0100

    Updated example for memdb

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,26 +1,26 @@
 extern crate symbolserver;
 
 use std::env;
+use std::fs;
 use std::path::Path;
 
 use symbolserver::sdk::SdkProcessor;
+use symbolserver::memdb::MemDbBuilder;
 use symbolserver::Result;
 
 fn do_main() -> Result<()> {
     let args : Vec<_> = env::args().collect();
     let p = Path::new(&args[1]);
+    let out = fs::File::create("/tmp/test.memdb")?;
     let sdk_proc = SdkProcessor::new(p)?;
-    println!("{:?}", sdk_proc.info());
+    let mut builder = MemDbBuilder::new(out, sdk_proc.info())?;
+
     for obj_res in sdk_proc.objects()? {
         let (filename, obj) = obj_res?;
-        for var in obj.variants() {
-            println!("  {} {:?} [{}]", filename, var.name(), var.arch());
-            let mut syms = obj.symbols(var.arch())?;
-            for (addr, sym) in syms.iter() {
-                println!("    {:016x}  {}", addr, sym);
-            }
-        }
+        builder.write_object(&obj, Some(&filename))?;
     }
+    builder.flush()?;
+
     Ok(())
 }
 
