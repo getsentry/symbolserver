408fc0838785cda1dfefab259dec6e27baad18ff
Armin Ronacher <armin.ronacher@active-4.com>
2017-02-21 00:43:38 +0100

    Also write out symbols in example

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,6 +4,7 @@ extern crate symbolserver;
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::io::Write;
 
 use uuid::Uuid;
 
@@ -17,10 +18,18 @@ fn do_main() -> Result<()> {
         let p = Path::new(&args[1]);
         let out = fs::File::create("/tmp/test.memdb")?;
         let sdk_proc = SdkProcessor::new(p)?;
+        let mut symout = fs::File::create("/tmp/symbols")?;
         let mut builder = MemDbBuilder::new(out, sdk_proc.info())?;
 
         for obj_res in sdk_proc.objects()? {
             let (filename, obj) = obj_res?;
+            for var in obj.variants() {
+                let mut symbols = obj.symbols(var.arch())?;
+                for (_, sym) in symbols.iter() {
+                    symout.write_all(sym.as_bytes())?;
+                    symout.write_all(b"\n")?;
+                }
+            }
             builder.write_object(&obj, Some(&filename))?;
         }
         builder.flush()?;
