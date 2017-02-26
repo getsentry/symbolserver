fe056f124b4f2726e66328a1846e34d11b7e5d73
Armin Ronacher <armin.ronacher@active-4.com>
2017-02-26 23:58:46 +0100

    Renamed some vars

--- src/api/handlers.rs
+++ src/api/handlers.rs
@@ -4,15 +4,12 @@ use hyper::method::Method;
 use uuid::Uuid;
 
 use super::super::{Result, ErrorKind};
-use super::super::sdk::SdkInfo;
 use super::server::{ServerContext, load_request_data};
 use super::types::{ApiResponse, ApiError};
 
 #[derive(Deserialize)]
 struct SymbolQuery {
     addr: u64,
-    image_addr: u64,
-    image_vmaddr: Option<u64>,
     image_uuid: Option<Uuid>,
     image_path: Option<String>,
 }
@@ -69,15 +66,15 @@ pub fn lookup_symbol_handler(ctx: &ServerContext, mut req: Request) -> Result<Ap
     };
 
     let mut rv = vec![];
-    for symbol_query in data.symbols {
+    for symq in data.symbols {
         let mut rvsym = None;
-        if let Some(ref uuid) = symbol_query.image_uuid {
-            if let Some(sym) = sdk.lookup_by_uuid(uuid, symbol_query.addr) {
+        if let Some(ref uuid) = symq.image_uuid {
+            if let Some(sym) = sdk.lookup_by_uuid(uuid, symq.addr) {
                 rvsym = Some(sym);
             }
-        } else if let Some(ref name) = symbol_query.image_path {
+        } else if let Some(ref name) = symq.image_path {
             if let Some(sym) = sdk.lookup_by_object_name(
-                name, &data.cpu_name, symbol_query.addr)
+                name, &data.cpu_name, symq.addr)
             {
                 rvsym = Some(sym);
             }
