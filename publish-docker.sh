9dca3a2687805cd95c02d6d5172b84bbe0198f85
Matt Robenolt <matt@ydekproductions.com>
2017-03-06 14:44:17 -0800

    Add script to publish to Docker Hub

---
+++ publish-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+
+aliases='0 latest'
+tag='getsentry/symbolserver'
+
+fullVersion=$(awk '$1 == "ENV" && $2 == "SYMBOLSERVER_VERSION" { print $3; exit }' Dockerfile)
+
+docker build --pull --rm -t $tag:$fullVersion .
+docker push $tag:$fullVersion
+
+for alias in $aliases; do
+    docker tag $tag:$fullVersion $tag:$alias
+    docker push $tag:$alias
+done
