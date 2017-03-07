ffccddad61da5dbc6f1ba20b054290d297ddd070
Armin Ronacher <armin.ronacher@active-4.com>
2017-03-07 13:55:59 +0000

    Try to switch to japaric/trust based deploys

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,7 +1,25 @@
-#!/bin/bash
+#!/bin/sh
+# This script takes care of testing your crate
+
 set -ex
 
-rustc --version
-cargo --version
+# TODO This is the "test phase", tweak it as you see fit
+main() {
+    cross build --target $TARGET
+    cross build --target $TARGET --release
 
-cargo build --release --target=$TARGET
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+
+    cross test --target $TARGET
+    cross test --target $TARGET --release
+
+    cross run --target $TARGET
+    cross run --target $TARGET --release
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
