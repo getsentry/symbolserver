fb714a274ee484ea5f54d154e1d15d5908d8fbb9
Armin Ronacher <armin.ronacher@active-4.com>
2017-03-08 23:11:16 +0000

    Always trigger builds

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,28 +1,3 @@
 #!/bin/sh
-# This script takes care of testing your crate
-
 set -ex
-
-# dummy c++ compiler :)
-export CXX=true
-
-# TODO This is the "test phase", tweak it as you see fit
-main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
-
-    if [ ! -z $DISABLE_TESTS ]; then
-        return
-    fi
-
-    cross test --target $TARGET
-    cross test --target $TARGET --release
-
-    cross run --target $TARGET
-    cross run --target $TARGET --release
-}
-
-# we don't run the "test phase" when doing deploys
-if [ -z $TRAVIS_TAG ]; then
-    main
-fi
+cross build --target $TARGET --release
